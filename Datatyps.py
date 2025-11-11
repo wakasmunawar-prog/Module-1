e7ea9f5eb34a9e29b6766aaf7c54e8378e695033
wakasmunawar-prog <wakasmunawar@gmail.com>
2025-11-11 19:59:54 +0400

    commit4

--- Datatyps.py
+++ Datatyps.py
@@ -6,3 +6,15 @@ c= "coding"
 print("type of c: ", type(c))
 d= True
 print("type of d: ", type(d))
+name = "Ammar"
+age = 10
+is_student = True
+weight = 38.5
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age : ", age)
+print("Data Type of Age is" ,type(age))
+print("is_student :", is_student)
+print("Data Type for is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is ", type(weight))
