60ea2ad6c1ab47dc1d4a3506062f43f2a015fcfa
wakasmunawar-prog <wakasmunawar@gmail.com>
2025-11-17 17:56:16 +0400

    commit6

---
+++ activity2.py
@@ -0,0 +1,13 @@
+name = "Ammar"
+Class = "6C"
+age = 10
+bestfriendname1 = "Yahya"
+bestfriendname2 = "Yazan"
+bestfriendname3 = "and Mira"
+print("My name is", name,"and i am", age, "years old", "and my best friends names are", bestfriendname1, bestfriendname2, bestfriendname3)
+
+a = 676767
+b = 56453
+c = 17
+d = 414141
+print(a + b + c + d)
