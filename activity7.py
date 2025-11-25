3cd470aa96e970f14ff071965a3b19debb5ec2f1
wakasmunawar-prog <wakasmunawar@gmail.com>
2025-11-25 19:51:44 +0400

    commit11

---
+++ activity7.py
@@ -0,0 +1,9 @@
+age = 10
+height = 130
+if age >= 10:
+    if height >= 120:
+        print("You can go on the ride")
+    else:
+        print("you are not tall enough")
+else:
+    print("You are not old enough for the ride")
